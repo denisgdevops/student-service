618e51b74a42ca4de9e196907a10e901b9a70bb4
gikundi <denisgikundi05@gmail.com>
2024-01-23 07:41:14 +0300

    initial code commit

---
+++ appp/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
@@ -0,0 +1,41 @@
+package com.dev.kali.apiservice.controller;
+
+
+import com.dev.kali.apiservice.dtos.Students;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+public class ApiController {
+
+    @GetMapping
+    public ResponseEntity<List<Students>> getStudents () {
+
+        List<Students> students = new ArrayList<>();
+
+        Students student1 = new Students();
+        student1.setId(1);
+        student1.setName("Robert Kamaru");
+        student1.setSex("Male");
+        student1.setAge(18);
+
+        Students student2 = new Students();
+        student2.setId(2);
+        student2.setName("Agnes B");
+        student2.setSex("Female");
+        student2.setAge(15);
+
+        students.add(student1);
+        students.add(student2);
+
+        return ResponseEntity.ok(students);
+
+    }
+
+}
