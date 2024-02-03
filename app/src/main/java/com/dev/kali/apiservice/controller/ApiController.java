5648560b679445446f954daa495342022a274861
gikundi <denisgikundi05@gmail.com>
2024-02-03 17:43:00 +0300

    updated API service

--- app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
+++ app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
@@ -21,15 +21,15 @@ public class ApiController {
 
         Students student1 = new Students();
         student1.setId(1);
-        student1.setName("Lawrence Kamethe");
+        student1.setName("James Katene");
         student1.setSex("Male");
-        student1.setAge(18);
+        student1.setAge(38);
 
         Students student2 = new Students();
         student2.setId(2);
-        student2.setName("Silvia Basila");
+        student2.setName("Kilonzi BKabasila");
         student2.setSex("Female");
-        student2.setAge(15);
+        student2.setAge(45);
 
         students.add(student1);
         students.add(student2);
