231d559540cb9e833674891f572b09fcc5af99ae
gikundi <denisgikundi05@gmail.com>
2024-02-07 19:43:01 +0300

    updated api request

--- app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
+++ app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
@@ -21,7 +21,7 @@ public class ApiController {
 
         Students student1 = new Students();
         student1.setId(1);
-        student1.setName("Philip Karwete");
+        student1.setName("Philip Kanji");
         student1.setSex("Male");
         student1.setAge(18);
 
---
+++ platform/helm/update-values.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Your logic to update values in values.yaml
+NEW_TAG="latest-$(git rev-parse --short HEAD)"
+
+sed -i "s/tag: latest/tag: $NEW_TAG/" values.yaml
+
+# Add the changes to the staging area
+git add values.yaml
+
+# Exit with success
+exit 0
+
