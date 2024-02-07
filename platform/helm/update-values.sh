bee358af20c960454a9fb48fdaa0337af27a9e20
gikundi <denisgikundi05@gmail.com>
2024-02-07 20:40:17 +0300

    updated to kalonzi

--- platform/helm/update-values.sh
+++ platform/helm/update-values.sh
@@ -3,7 +3,7 @@
 # Your logic to update values in values.yaml
 NEW_TAG="$(git rev-parse --short HEAD)"
 
-sed -i '/^ *tag:/s/latest.*$/latest'"$NEW_TAG"'/' values.yaml
+sed -i 's/^\(\s*tag:\s*\).*/\1'"$NEW_TAG"'/' values.yaml
 
 # Add the changes to the staging area
 git add values.yaml
