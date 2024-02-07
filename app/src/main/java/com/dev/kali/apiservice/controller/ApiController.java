eecf709944a232d1ea2fabc54ff62fa53096ae47
gikundi <denisgikundi05@gmail.com>
2024-02-07 14:03:31 +0300

    updated api values

--- app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
+++ app/src/main/java/com/dev/kali/apiservice/controller/ApiController.java
@@ -21,7 +21,7 @@ public class ApiController {
 
         Students student1 = new Students();
         student1.setId(1);
-        student1.setName("Philip Machanta");
+        student1.setName("Philip Kanyanja");
         student1.setSex("Male");
         student1.setAge(18);
 
